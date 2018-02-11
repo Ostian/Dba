214b592e2480a25011f53bc15ce918a3b89c911e
Laboratorio2 <stiang887@gmail.com>
2018-02-11 16:30:27 -0600

    probando

--- index.php
+++ index.php
@@ -1,8 +1,32 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Laboratorio2
- * Date: 11/2/2018
- * Time: 15:10
- */
-echo "hi";
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> mi pagina </title>
+</head>
+<body>
+
+<header style="background-color: black; width: 100%; height: 200px;">
+    <p>Soy un Parrafo</p>
+    <section style="background-color: blue; width: 100%; height: 50px;"></section>
+    <nav style="background-color: orange; width: 100%; height: 60px"></nav>
+
+</header>
+<section>
+    <h1>hola soy un parrafo</h1>
+</section>
+<section>
+    <h1>hola soy un parrafo</h1>
+</section>
+<section>
+    <h1>hola soy un parrafo</h1>
+</section>
+<section>
+    <h1>hola soy un parrafo</h1>
+</section>
+section*5>h1
+</body>
+</html>
